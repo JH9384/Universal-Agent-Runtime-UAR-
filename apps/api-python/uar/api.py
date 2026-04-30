ab82d55133cb889b1cccc427bdcd0c318db81ee5
JH9384 <54092180+JH9384@users.noreply.github.com>
2026-04-29 20:18:39 -0400

    Extract api layer

---
+++ apps/api-python/uar/api.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from .models import UORObjectIn
+from .store import create_record
+
+app = FastAPI()
+
+@app.post("/objects")
+def create(obj: UORObjectIn):
+    return create_record(obj.mediaType, obj.mode, obj.attributes, obj.links, obj.content)
